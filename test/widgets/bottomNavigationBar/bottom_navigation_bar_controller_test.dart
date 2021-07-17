99deac8b31c270caa2eecb5d5622c4be16f7eca0
cheese <cheese@odds.team>
2021-07-17 21:03:19 +0700

    remove test

--- test/widgets/bottomNavigationBar/bottom_navigation_bar_controller_test.dart
+++ test/widgets/bottomNavigationBar/bottom_navigation_bar_controller_test.dart
@@ -11,20 +11,8 @@ void main() {
 
     test("should set current index when tap bottom navigation bar", () {
       var controller = BottomNavigationBarController();
-      var view = MockView();
-      controller.setView(view);
       controller.setCurrentTabIndex(1);
       expect(controller.getCurrentTabIndex(), 1);
     });
-
-    test("should route to given path", () {
-      var controller = BottomNavigationBarController();
-      var view = MockView();
-      controller.setView(view);
-      controller.setCurrentTabIndex(4);
-      verify(view.changeTab("/profile")).called(1);
-    });
   });
 }
-
-class MockView extends Mock implements BottomNavigationBarView {}
