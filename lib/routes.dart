3c631425b27a6277cdfdba24600eeb8a663d42a9
cheese <cheese@odds.team>
2021-07-17 20:37:16 +0700

    add profile page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,11 @@
 import 'package:app/pages/home/home.dart';
 import 'package:app/pages/home/home_binding.dart';
+import 'package:app/pages/profile/profile.dart';
 import 'package:get/route_manager.dart';
 
 class Routes {
   static const home = "/home";
+  static const profile = "/profile";
 
   static List<GetPage> getPages() {
     return [
@@ -11,6 +13,10 @@ class Routes {
         name: home,
         page: () => Home(),
         binding: HomeBinding(),
+      ),
+      GetPage(
+        name: profile,
+        page: () => Profile(),
       )
     ];
   }
