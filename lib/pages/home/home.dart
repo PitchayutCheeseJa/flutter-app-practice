5cd25a95e92352b64502d551c2b4f145b5d8633e
cheese <cheese@odds.team>
2021-07-17 19:13:33 +0700

    add simple bottom navigation bar

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:app/widgets/bottomNavigationBar/bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +7,9 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(),
+      bottomNavigationBar: AppBottomNavigationBar(),
+    );
   }
 }
